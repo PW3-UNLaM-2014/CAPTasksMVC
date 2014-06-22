0cfbc70395c6d2c8a1240f848b69ea4182d426e4
CeGisele <cegisele@hotmail.com>
2014-06-22 19:39:15 -0300

    Iniciando proyecto

---
+++ CAPTasksMVC/CAPTasksMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CAPTasksMVC.Models;
+
+namespace CAPTasksMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CAPTasksEntities cap = new CAPTasksEntities();
+        
+        //
+        // GET: /Home/
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult CrearCarpeta()
+        {
+            return View();
+        }
+
+        public ActionResult CrearTarea()
+        {
+            ViewBag.IdCarpeta = new SelectList(cap.Carpetas, "IdCarpeta", "Nombre");
+            ViewBag.IdUsuario = new SelectList(cap.Usuarios, "IdUsuario", "Nombre");
+            return View();
+        }
+
+        public ActionResult ModificarTarea()
+        {
+            return View();
+        }
+
+        public ActionResult EliminarTarea()
+        {
+            return View();
+        }
+    }
+}
---
+++ CAPTasksMVC/CAPTasksMVC/Controllers/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CAPTasksMVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
