2e7d71f471ed6271cf9e3e0fa21043bae49243a0
AgoFig <agostinafigueredo@gmail.com>
2014-07-02 23:34:01 -0300

    Preparando sistema de logueo y sesion

--- CAPTasksMVC/CAPTasksMVC/Controllers/IndexController.cs
+++ CAPTasksMVC/CAPTasksMVC/Controllers/IndexController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.Mvc;
+using CAPTasksMVC.Models;
 
 namespace CAPTasksMVC.Controllers
 {
@@ -13,7 +15,55 @@ namespace CAPTasksMVC.Controllers
 
         public ActionResult Login()
         {
-            return View();
+            Usuarios user = new Usuarios();
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Login(Usuarios model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                using (CAPTasksEntities entities = new CAPTasksEntities()) {
+                    string username = model.Nombre;
+                    string password = model.Contrasenia;
+
+                    PasswordManagement criptografia = new PasswordManagement();
+
+                    password = criptografia.Encrypt(password);
+                    
+                    bool userValid = entities.Usuarios.Any(user => user.Nombre == username && user.Contrasenia == password);
+
+                    if (userValid)
+                    {
+                        FormsAuthentication.SetAuthCookie(username, false);
+
+                        if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                            && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                        {
+                            return Redirect(returnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "El nombre de usuario o la contrase&ntilde;a no son correctos.");
+                        
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
 
     }
