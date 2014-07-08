fe80d240072b452cba973d9b1771211a7b3aa913
AgoFig <agostinafigueredo@gmail.com>
2014-07-08 19:59:49 -0300

    Ahora la url se genera con puerto

--- CAPTasksMVC/CAPTasksMVC/Servicios/MailsServicios.cs
+++ CAPTasksMVC/CAPTasksMVC/Servicios/MailsServicios.cs
@@ -15,30 +15,33 @@ namespace CAPTasksMVC.Servicios
 
         SmtpClient client = new SmtpClient();
 
-        public void EnviarMail(Usuarios model) {
+        public void EnviarMail(Usuarios model)
+        {
 
-            UriBuilder uri = new UriBuilder();
-                //ENVIO DE MAIL:
-                msj.To.Add(new MailAddress(model.Email));
-                msj.From = new MailAddress("nuestra.aplicacion2014@gmail.com");
-                msj.Subject = "Activación de cuenta";
-                msj.SubjectEncoding = System.Text.Encoding.UTF8;
-                string body = "Hola " + model.Nombre.Trim() + ",";
-                body += "<br/><br/>Por favor, haga click en el siguiente link para activar su cuenta:<br/>";
-                body += "<br /><a href='"+ uri +"Cuenta/Activar/"
-                    + model.CodigoActivacion 
-                    + "'>Haga click aqui para activar su cuenta</a>";
-                body += "<br /><br />Muchas gracias, CAPTasks!";
-                msj.Body = body;
-                msj.IsBodyHtml = true;
+            //ENVIO DE MAIL:
+            msj.To.Add(new MailAddress(model.Email));
+            msj.From = new MailAddress("nuestra.aplicacion2014@gmail.com");
+            msj.Subject = "Activación de cuenta";
+            msj.SubjectEncoding = System.Text.Encoding.UTF8;
+            string body = "Hola " + model.Nombre.Trim() + ",";
+            body += "<br/><br/>Por favor, haga click en el siguiente link para activar su cuenta:<br/>";
+            body += "<br /><a href='" 
+                 + HttpContext.Current.Request.Url.Scheme
+                 + "://"
+                 + HttpContext.Current.Request.Url.Authority + "/Cuenta/Activar/"
+                 + model.CodigoActivacion
+                 + "'>Haga click aqui para activar su cuenta</a>";
+            body += "<br /><br />Muchas gracias, CAPTasks!";
+            msj.Body = body;
+            msj.IsBodyHtml = true;
 
-                client.Host = "smtp.gmail.com";
-                client.EnableSsl = true;
-                NetworkCredential netcred = new NetworkCredential("unlampw3", "unlampw1c-2014");
-                client.UseDefaultCredentials = true;
-                client.Credentials = netcred;
-                client.Port = 587;
-                client.Send(msj);
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+            NetworkCredential netcred = new NetworkCredential("unlampw3", "unlampw1c-2014");
+            client.UseDefaultCredentials = true;
+            client.Credentials = netcred;
+            client.Port = 587;
+            client.Send(msj);
         }
     }
 }
